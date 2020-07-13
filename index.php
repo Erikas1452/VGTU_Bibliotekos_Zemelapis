acfc1b7f21e2a94eb65df00e6676293b8bc4f1d4
Erikas1452 <62668639+Erikas1452@users.noreply.github.com>
2020-07-13 15:52:53 +0300

    added tabs for different maps (Library , auditorium, shelf);

--- index.php
+++ index.php
@@ -24,7 +24,8 @@ $second_floor = new Map("images/VGTUB_2a.png");
         <meta charset="utf-8">
         <title>VGTU Library Map</title>
         <!-- (ADD LATER)References to styles -->
-        <!-- <link href ="canvas.css" rel="stylesheet"> -->
+        <link href ="style.css" rel="stylesheet">
+        <script src="script.js" defer></script>
     </head>
 
     <body>
@@ -45,34 +46,41 @@ $second_floor = new Map("images/VGTUB_2a.png");
                     }
                     ?>
                 </datalist>
-                <?php $handler->displayButton("Ieškoti","Search"); ?>
+                <?php
+                $handler->displayButton("Ieškoti","Search");
+                $data->getValueFromSelection("Search","DropDown1");
+                $search_for = $data->returnSelectedValue();
+                echo '    Test :  Selected: '.$search_for; //Testing if the selected value is extracted correctly
+                ?>
             </form>
             <br>
+            <ul class="tabs">
+                <li data-tab-target="#biblioteka" class="active tab">Biblioteka</li>
+                <li data-tab-target="#auditorija" class="tab">Auditorija</li>
+                <li data-tab-target="#lentyna" class="tab">Lentyna</li>
+            </ul>
 
-            <?php
-            echo "aukštas ";
-            $handler->displayButton("Biblioteka","Biblioteka");
-            echo "  ";
-            $handler->displayButton("Auditorija","Auditorija");
-            echo "  ";
-            $handler->displayButton("Lentyna","Lentyna");
-            echo "  ";
+            <dvi class="tab-content">
+                <div id="biblioteka" data-tab-content class = 'active'>
+                    <?php
 
-            $data->getValueFromSelection("Search","DropDown1");
-            $search_for = $data->returnSelectedValue();
-            echo '    Test :  Selected: '.$search_for; //Testing if the selected value is extracted correctly
+                    //Filling all maps with markers
+                    $first_floor->fillMapByTheme($shelves_to_mark,$search_for);
+                    $second_floor->fillMapByTheme($shelves_to_mark,$search_for);
 
-            echo $themes[0][1];
-
-            //Filling all maps with markers
-            $first_floor->fillMapByTheme($shelves_to_mark,$search_for);
-            $second_floor->fillMapByTheme($shelves_to_mark,$search_for);
-
-            //Displaying map
-            $second_floor->saveMap('images/VGTUB_2a'.'_'.$search_for.'.png');
-            $handler->onButtonDisplayImage("Search",$second_floor->returnPath()); // <-- Needs a way to find out which map print-out
-            $handler->displayTable($themes);
-            ?>
+                    //Displaying map
+                    $second_floor->saveMap('images/VGTUB_2a'.'_'.$search_for.'.png');
+                    $handler->onButtonDisplayImage("Search",$second_floor->returnPath()); // <-- Needs a way to find out which map print-out
+                    $handler->displayTable($themes);
+                    ?>
+                </div>
+                <div id="auditorija" data-tab-content>
+                    <h3>Auditorijos zemelapis</h3>
+                </div>
+                <div id="lentyna" data-tab-content>
+                    <h3>Lentyna</h3>
+                </div>
+            </dvi>
         </div>
     </body>
 </html>
